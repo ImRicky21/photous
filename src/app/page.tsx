c32739339521454140788ec10a69357f8a54f868
ImRicky21 <rickyrahmad2121@gmail.com>
2025-03-06 18:00:45 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,19 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className="p-10">
-          <h1 className="text-center text-2xl font-bold font-bold">
+        <div className="m-4">
+          <h1 className="text-xl italic">
             Wellcome to{" "}
-            <span className="text-slate-400 text-4xl ">
-              &rdquo;Photous&rdquo;
+            <span className="text-slate-400 font-semibold text-4xl ">
+              &rdquo;PhotoUs&rdquo;
             </span>
           </h1>
+          <p className="italic text-slate-400">
+            {" "}
+            &rdquo;you and me in one photo&rdquo;
+          </p>
+        </div>
+        <div className="p-10">
           <CameraComponent />
         </div>
       </main>
